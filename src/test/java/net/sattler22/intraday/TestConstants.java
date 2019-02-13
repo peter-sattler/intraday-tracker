701e96e895e701d6e59a3d6bd16dd2a7607739d6
Peter Sattler <peter-sattler@users.noreply.github.com>
2019-02-13 18:25:33 -0500

    Create TestConstants.java

---
+++ src/test/java/net/sattler22/intraday/TestConstants.java
@@ -0,0 +1,32 @@
+package net.sattler22.intraday;
+
+import java.math.RoundingMode;
+
+/**
+ * Test Symbols and Constants
+ *
+ * @author Pete Sattler
+ * @version February 6, 2019
+ */
+public final class TestConstants {
+
+    /**
+     * Apple security symbol
+     */
+    public static final String APPLE = "APPL";
+
+    /**
+     * Facebook security symbol
+     */
+    public static final String FACEBOOK = "FB";
+
+    /**
+     * Google security symbol
+     */
+    public static final String GOOGLE = "GOOG";
+
+    /**
+     * Default rounding mode
+     */
+    public static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
+}
