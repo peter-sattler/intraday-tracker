6d0ee8fe960d19128d9b0d8cc3b55b90e6f3616e
Peter Sattler <peter@sattler22.net>
2026-05-24 20:26:18 -0400

    Upgraded to Java 25

--- src/main/java/net/sattler22/intraday/client/IntradayTrackingCommandLineClient.java
+++ src/main/java/net/sattler22/intraday/client/IntradayTrackingCommandLineClient.java
@@ -5,8 +5,6 @@ import net.sattler22.intraday.service.IntradayTrackingServiceInMemoryImpl;
 
 import java.io.Console;
 import java.math.RoundingMode;
-import java.util.Collection;
-import java.util.Objects;
 
 /**
  * Intraday Tracking Command-Line Client
@@ -15,22 +13,12 @@ import java.util.Objects;
  * @since February 12, 2019
  * @version May 2026
  */
-public record IntradayTrackingCommandLineClient(IntradayTrackingService intradayTrackingService) {
+public final class IntradayTrackingCommandLineClient {
 
-    private static final RoundingMode ROUNDING_MODE = RoundingMode.HALF_UP;
-    private static final String USER_PROMPT = "Enter {TRADE DATE (YYYY-MM-DD)} {SYMBOL} {PRICE} or quit to terminate";
     private static final String USER_TERMINATE = "quit";
 
-    public IntradayTrackingCommandLineClient {
-        Objects.requireNonNull(intradayTrackingService, "Intraday tracking service is required");
-    }
-
-    private void book(IntradayTrackingClientParser.TradeData tradeData) {
-        intradayTrackingService.book(tradeData.tradeDate(), tradeData.symbol(), tradeData.price());
-    }
-
-    private Collection<IntradayTrackingService.Security> securities() {
-        return intradayTrackingService.securities();
+    private IntradayTrackingCommandLineClient() {
+        throw new AssertionError("Class cannot be instantiated");
     }
 
     /**
@@ -42,12 +30,10 @@ public record IntradayTrackingCommandLineClient(IntradayTrackingService intraday
             System.err.println("Please run from a terminal/command prompt");
             return;
         }
-        console.printf("*** Intraday Tracker Command-Line Client (Rounding mode: [%s]) ***%n", ROUNDING_MODE.name());
+        console.printf("*** Intraday Tracker Command-Line Client ***%n");
         final IntradayTrackingService intradayTrackingService = new IntradayTrackingServiceInMemoryImpl();
-        final IntradayTrackingCommandLineClient intradayTrackingClient =
-                new IntradayTrackingCommandLineClient(intradayTrackingService);
         while (true) {
-            console.printf("%s%n> ", USER_PROMPT);
+            console.printf("Enter {TRADE DATE (YYYY-MM-DD)} {SYMBOL} {PRICE} or quit to terminate%n> ");
             final String userInput = console.readLine();
             if (userInput == null || USER_TERMINATE.equalsIgnoreCase(userInput.strip())) {
                 console.printf("Intraday Tracker Command-Line Client terminated%n");
@@ -55,20 +41,15 @@ public record IntradayTrackingCommandLineClient(IntradayTrackingService intraday
             }
             try {
                 IntradayTrackingClientParser.parse(userInput).ifPresent(tradeData -> {
-                    intradayTrackingClient.book(tradeData);
-                    displayResults(console, intradayTrackingClient.securities());
+                    intradayTrackingService.book(tradeData.tradeDate(), tradeData.symbol(), tradeData.price());
+                    intradayTrackingService.securities().forEach(security ->
+                        console.printf("< %s %s %s %s %s%n", security.tradeDate(), security.symbol(), security.highPrice(),
+                                security.lowPrice(), security.calcAveragePrice(2, RoundingMode.HALF_UP)));
                 });
             }
             catch (RuntimeException runtimeException) {
                 runtimeException.printStackTrace(System.err);
             }
-        }
-    }
-
-    private static void displayResults(Console console, Collection<IntradayTrackingService.Security> securities) {
-        for (final IntradayTrackingService.Security security : securities) {
-            console.printf("< %s %s %s %s %s%n", security.tradeDate(), security.symbol(), security.highPrice(),
-                    security.lowPrice(), security.calcAveragePrice(2, ROUNDING_MODE));
         }
     }
 }
